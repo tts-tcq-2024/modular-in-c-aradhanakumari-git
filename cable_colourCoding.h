e06d0ba2a1d87e2031a239b9a732256ec8b6dd8e
aradhanakumari-git <aradhana.kumari2710@gmail.com>
2024-07-18 10:21:27 +0530

    Update cable_colourCoding.h

--- cable_colourCoding.h
+++ cable_colourCoding.h
@@ -9,6 +9,24 @@ typedef struct {
     enum MinorColor minorColor;
 } ColorPair;
 
+const char* MajorColorNames[] = {
+    "White", "Red", "Black", "Yellow", "Violet"
+};
+const char* MinorColorNames[] = {
+    "Blue", "Orange", "Green", "Brown", "Slate"
+};
+int numberOfMajorColors =
+    sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+
+int numberOfMinorColors =
+    sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+
+void ColorPairToString(const ColorPair* colorPair, char* buffer) {
+    sprintf(buffer, "%s %s",
+        MajorColorNames[colorPair->majorColor],
+        MinorColorNames[colorPair->minorColor]);
+}
+const int MAX_COLORPAIR_NAME_CHARS = 16;
 void testPairToNumber(
     enum MajorColor major,
     enum MinorColor minor,
