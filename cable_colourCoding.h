0a09051d98e57a1c44a18d8160e5af324452d0d9
aradhanakumari-git <aradhana.kumari2710@gmail.com>
2024-07-18 10:06:39 +0530

    Update cable_colourCoding.h

--- cable_colourCoding.h
+++ cable_colourCoding.h
@@ -4,9 +4,7 @@
 enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
 enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
 
-const char* MajorColorNames[] = {
-    "White", "Red", "Black", "Yellow", "Violet"
-};
+
 int numberOfMajorColors =
     sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
 const char* MinorColorNames[] = {
