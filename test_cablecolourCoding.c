719c1798a071d2f6d14aa1822fd409babb51a19a
aradhanakumari-git <aradhana.kumari2710@gmail.com>
2024-08-01 00:20:48 +0530

    Update test_cablecolourCoding.c

--- test_cablecolourCoding.c
+++ test_cablecolourCoding.c
@@ -25,3 +25,13 @@ void testPairToNumber(
     printf("Got pair number %d\n", pairNumber);
     assert(pairNumber == expectedPairNumber);
 }
+
+int main() {
+    testNumberToPair(4, WHITE, BROWN);
+    testNumberToPair(5, WHITE, SLATE);
+
+    testPairToNumber(BLACK, ORANGE, 12);
+    testPairToNumber(VIOLET, SLATE, 25);
+
+    return 0;
+}
